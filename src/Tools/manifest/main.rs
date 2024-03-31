21fb6ca75633717c05f73c375ce749e3cce51e07
Darrion Whitfield <dwhitfield@ramfield.net>
2024-03-31 03:24:54 -0500

    Revert "Rebased RC"
    This reverts commit a0271d95e8b7f559f697cd4b4efba1a676f9390a.

---
+++ src/Tools/manifest/main.rs
@@ -0,0 +1,32 @@
+use pretty::pass;
+use shared::{ais_data::AisInfo, errors::UnifiedError};
+use system::{create_hash, truncate};
+
+fn main() -> Result<(), UnifiedError> {
+    // Create an instance of AisInfo
+    let mut ais_info: AisInfo = AisInfo::new()?;
+
+    ais_info.machine_id = Some(
+        truncate(
+            &create_hash(format!(
+                "{}{}",
+                &ais_info
+                    .clone()
+                    .machine_ip
+                    .unwrap_or(String::from("Uninitialized")),
+                &ais_info
+                    .clone()
+                    .machine_id
+                    .unwrap_or(String::from("Uninitialized"))
+            )),
+            16,
+        )
+        .to_owned(),
+    );
+    // Generate the manifest file
+    ais_info.create_manifest()?;
+
+    pass("Manifest file created successfully");
+
+    Ok(())
+}
